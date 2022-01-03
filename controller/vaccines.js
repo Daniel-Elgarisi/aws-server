7d4fec5743bd8a3854f595258258d5606ed84889
Daniel-Elgarisi <danielelgarisi7@gmail.com>
2022-01-03 17:48:04 +0200

    Back files

--- controller/vaccines.js
+++ controller/vaccines.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/watchVacc/:phonenumber/:petname', getVaccines);
+router.post('/updateVaccein', updateVaccines);
 
 async function getVaccines(req, response) {
 
@@ -33,5 +34,32 @@ async function getVaccines(req, response) {
         }
     });
 }
+
+async function updateVaccines(req, response) {
+
+    let user = await client.query("select * from users where phone_number=$1", [req.body.phone_number]);
+    if (user.rowCount == 0) {
+        return response.status(400).json({ message: "user is not found" });
+    }
+
+    let userpets = await client.query('select * from pets where owner_id=$1 AND name=$2', [user.rows[0].id, req.body.petname]);
+    if (userpets.rowCount == 0) {
+        return response.status(400).json({ message: "pet is not found" });
+    }
+
+    let sql = `INSERT INTO vaccantions (vaccine_type, pet_id, date) VALUES($1, $2, CURRENT_DATE)`;
+    let vars = [req.body.vaccine_type, userpets.rows[0].id];
+
+    client.query(sql, vars, (err, res) => {
+        if (err) {
+            console.log(err);
+            return response.status(400).json({ message: "Somthing wrong" });
+        } else {
+            return response.status(200).json({ message: "vaccines is created successfully" });
+        }
+    });
+}
+
+
 module.exports = router;
 
